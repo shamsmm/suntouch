0efd56a3276dc946398b99e38b3b9b32dc933627
Shams M.Monem <shamsmmonem@gmail.com>
2022-10-01 09:59:50 +0200

    -

--- src/views/quiz/_questions.js
+++ src/views/quiz/_questions.js
@@ -1,44 +1,40 @@
 const questions = [
     {
-        title: "Question 1: (Correct Answer is C)",
+        title: "Why parker solar probe is orbiting around venus",
         answers: [
-            {title: "Answer A", letter: "A"},
-            {title: "Answer B", letter: "B"},
-            {title: "Answer C", letter: "C"},
-            {title: "Answer D", letter: "D"},
-        ],
-        correct: "C"
-    },
-    {
-        title: "Question 2: (Correct Answer is D)",
-        answers: [
-            {title: "Answer A", letter: "A"},
-            {title: "Answer B", letter: "B"},
-            {title: "Answer C", letter: "C"},
-            {title: "Answer D", letter: "D"},
-        ],
-        correct: "D"
-    },
-    {
-        title: "Question 3: (Correct Answer is C)",
-        answers: [
-            {title: "Answer A", letter: "A"},
-            {title: "Answer B", letter: "B"},
-            {title: "Answer C", letter: "C"},
-            {title: "Answer D", letter: "D"},
-        ],
-        correct: "C"
-    },
-    {
-        title: "Question 4: (Correct Answer is A)",
-        answers: [
-            {title: "Answer A", letter: "A"},
-            {title: "Answer B", letter: "B"},
-            {title: "Answer C", letter: "C"},
-            {title: "Answer D", letter: "D"},
+            {title: "Increase it's Energy", letter: "A"},
+            {title: "The purpose was to discover venus in the first place", letter: "B"},
+            {title: "To lower the costs", letter: "C"},
         ],
         correct: "A"
-    }
+    },
+    {
+        title: "How did they overcome the temperature",
+        answers: [
+            {title: "The protective shield", letter: "A"},
+            {title: "Sun's surface corona is cool", letter: "B"},
+            {title: "Using solar panels", letter: "C"},
+        ],
+        correct: "A"
+    },
+    {
+        title: "Why parker should be light",
+        answers: [
+            {title: "It will be launched with high speed", letter: "A"},
+            {title: "Lower the costs", letter: "B"},
+            {title: "Not to be gulped by the sun", letter: "C"},
+        ],
+        correct: "A"
+    },
+    {
+        title: "The solution for the limited kinetic energy parker has",
+        answers: [
+            {title: "Using solar panels", letter: "A"},
+            {title: "Natural gas", letter: "B"},
+            {title: "Orbiting venus", letter: "C"},
+        ],
+        correct: "C"
+    },
 ];
 
 export default questions;
